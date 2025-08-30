839bf0ab41689af567e5fabdd97d40660576db44
Paul Hagedorn <panzer1119@gmail.com>
2025-08-30 15:36:24 +0200

    feat: add support for multiple audio streams in config

--- backend/download/src/yt_dlp_handler.py
+++ backend/download/src/yt_dlp_handler.py
@@ -170,6 +170,7 @@ class VideoDownloader(DownloaderBase):
         """build user customized options"""
         if self.config["downloads"]["format"]:
             self.obs["format"] = self.config["downloads"]["format"]
+            self.obs["allow_multiple_audio_streams"] = "language" in self.config["downloads"]["format"]
         if self.config["downloads"]["format_sort"]:
             format_sort = self.config["downloads"]["format_sort"]
             format_sort_list = [i.strip() for i in format_sort.split(",")]
